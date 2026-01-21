b73db941e9f5aa26d7a2d29ebed062aca054f756
Mathias Arlaud <mathias.arlaud@gmail.com>
2026-01-21 10:47:30 +0100

    [nvim] add gitsigns stage/undo keymap

--- nvim/lua/mtarld/plugins/gitsigns.lua
+++ nvim/lua/mtarld/plugins/gitsigns.lua
@@ -5,6 +5,8 @@ return {
   },
   event = "BufRead",
   keys = {
+    { "<leader>gs", "<CMD>Gitsigns stage_hunk<CR>" },
+    { "<leader>gu", "<CMD>Gitsigns undo_stage_hunk<CR>" },
     { "<leader>gr", "<CMD>Gitsigns reset_hunk<CR>" },
     { "<leader>gp", "<CMD>Gitsigns preview_hunk<CR>" },
   },
