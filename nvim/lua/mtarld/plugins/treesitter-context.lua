66432440fa681c39828dc0a4b2afb2b9e10c5315
Mathias Arlaud <mathias.arlaud@gmail.com>
2026-03-30 16:21:39 +0200

    [nvim] fix treesitter temporarly

--- nvim/lua/mtarld/plugins/treesitter-context.lua
+++ nvim/lua/mtarld/plugins/treesitter-context.lua
@@ -4,4 +4,20 @@ return {
     max_lines = 1,
   },
   event = "BufRead",
+  config = function(_, opts)
+    local context = require("treesitter-context")
+    context.setup(opts)
+
+    -- Workaround for Neovim 0.12 race condition where treesitter parse()
+    -- can produce nil nodes, causing range/assertion errors (neovim/neovim#38461)
+    local ctx = require("treesitter-context.context")
+    local original_get = ctx.get
+    ctx.get = function(...)
+      local ok, ranges, lines = pcall(original_get, ...)
+      if ok then
+        return ranges, lines
+      end
+      return nil, nil
+    end
+  end,
 }
--- nvim/lua/mtarld/plugins/treesitter.lua
+++ nvim/lua/mtarld/plugins/treesitter.lua
@@ -5,6 +5,18 @@ return {
   },
   event = "BufRead",
   config = function()
+    -- Workaround for Neovim 0.12 race condition where parse() yields nil
+    -- nodes causing 'range' nil errors (neovim/neovim#38461)
+    local lt = require("vim.treesitter.languagetree")
+    local original_parse = lt.parse
+    lt.parse = function(self, ...)
+      local ok, result = pcall(original_parse, self, ...)
+      if ok then
+        return result
+      end
+      return {}
+    end
+
     require("nvim-treesitter.configs").setup({
       ensure_installed = {
         "bash",
